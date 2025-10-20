d767405e0ea515f2b049d8f83f1aac3b4621fc8f
Jaden M <jrmantie@hotmail.com>
2025-10-20 16:53:26 -0600

    Added models / schemas for games and teams

---
+++ py-api/app/schemas/game_schema.py
@@ -0,0 +1,31 @@
+# Created 2025/10/20
+# This file handles structuring the Game table for our database
+import sqlalchemy as sa
+from sqlalchemy import Column
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine, event
+
+import app.config
+
+Base = declarative_base()
+
+class Game(Base):
+    __tablename__ = "Game"
+    id = Column(sa.Integer, primary_key=True, index=True, nullable=False, autoincrement=True)
+    gameDate = Column(sa.Date, nullable=False)
+    homeTeam = Column(sa.Integer, sa.ForeignKey("Team.id"), index=True, nullable=False)
+    awayTeam = Column(sa.Integer, sa.ForeignKey("Team.id"), index=True, nullable=False)
+    homeScore = Column(sa.Integer, nullable=False)
+    awayScore = Column(sa.Integer, nullable=False)
+
+
+engine = create_engine(app.config.DB_PATH, echo=True, future=True)
+
+# ensures that SQLite will enforce the foreign keys to the Team table
+@event.listens_for(engine, "connect")
+def _set_sqlite_pragma(dbapi_connection):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON;")
+    cursor.close()
+
+Base.metadata.create_all(engine)
